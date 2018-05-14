155aca59d0ec1a0682f02eadff5e66361741fe8b
lisyaoran51 <lisyaoran51@hotmail.com>
2018-05-15 00:12:16 +0800

    Update NeoKB.cpp

--- NeoKB.cpp
+++ NeoKB.cpp
@@ -11,7 +11,7 @@
 #define STRIP_TYPE              WS2811_STRIP_GBR		// WS2812/SK6812RGB integrated chip+leds
 //#define STRIP_TYPE            SK6812_STRIP_RGBW		// SK6812RGBW (NOT SK6812RGB)
 
-#define WIDTH                   8
+#define WIDTH                   24
 #define HEIGHT                  1
 #define LED_COUNT               (WIDTH * HEIGHT)
 
