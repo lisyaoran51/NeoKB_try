c3b7486872764bc751c18da575ed4feda990537a
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2018-06-14 15:54:48 +0800

    no message

--- NeoKB.cpp
+++ NeoKB.cpp
@@ -18,107 +18,10 @@
 ws2811_t ledstring = {};
 
 
-<<<<<<< HEAD
 int main(int argc, char *argv[])
 {
-	
 	getListOfDir();
 	
-	ws2811_return_t ret;
-
-	ledstring.freq = TARGET_FREQ;
-	ledstring.dmanum = DMA;
-	ledstring.channel[0].gpionum = GPIO_PIN;
-	ledstring.channel[0].count = LED_COUNT;
-	ledstring.channel[0].invert = 0;
-	ledstring.channel[0].brightness = 255;
-	ledstring.channel[0].strip_type = STRIP_TYPE;
-	ledstring.channel[1].gpionum = 0;
-	ledstring.channel[1].count = 0;
-	ledstring.channel[1].invert = 0;
-	ledstring.channel[1].brightness = 0;
-
-	ws2811_led_t dotcolors[] =
-	{
-		0x00200000,  // red
-		0x00201000,  // orange
-		0x00202000,  // yellow
-		0x00002000,  // green
-		0x00002020,  // lightblue
-		0x00000020,  // blue
-		0x00100010,  // purple
-		0x00200010,  // pink
-	};
-
-	if ((ret = ws2811_init(&ledstring)) != WS2811_SUCCESS)
-	{
-		fprintf(stderr, "ws2811_init failed: %s\n", ws2811_get_return_t_str(ret));
-		return ret;
-	}
-
-	while (1) {
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				ledstring.channel[0].leds[j] = dotcolors[i];
-				//ledstring.channel[0].leds[j] = 0x00201000;
-			}
-			if ((ret = ws2811_render(&ledstring)) != WS2811_SUCCESS)
-			{
-				fprintf(stderr, "ws2811_render failed: %s\n", ws2811_get_return_t_str(ret));
-				break;
-			}
-			//printf("render %d time...", i);
-
-			// 15 frames /sec
-			//usleep(1000000 / 15);
-		}
-	}
-
-
-
-	//sprintf(VERSION, "%d.%d.%d", VERSION_MAJOR, VERSION_MINOR, VERSION_MICRO);
-	//
-	//parseargs(argc, argv, &ledstring);
-	//
-	//matrix = malloc(sizeof(ws2811_led_t) * width * height);
-	//
-	//setup_handlers();
-	//
-	//if ((ret = ws2811_init(&ledstring)) != WS2811_SUCCESS)
-	//{
-	//	fprintf(stderr, "ws2811_init failed: %s\n", ws2811_get_return_t_str(ret));
-	//	return ret;
-	//}
-	//
-	//while (running)
-	//{
-	//	matrix_raise();
-	//	matrix_bottom();
-	//	matrix_render();
-	//
-	//	if ((ret = ws2811_render(&ledstring)) != WS2811_SUCCESS)
-	//	{
-	//		fprintf(stderr, "ws2811_render failed: %s\n", ws2811_get_return_t_str(ret));
-	//		break;
-	//	}
-	//
-	//	// 15 frames /sec
-	//	usleep(1000000 / 15);
-	//}
-	//
-	//if (clear_on_exit) {
-	//	matrix_clear();
-	//	matrix_render();
-	//	ws2811_render(&ledstring);
-	//}
-	//
-	//ws2811_fini(&ledstring);
-	//
-	//printf("\n");
-	return ret;
-=======
-int main(int argc, char *argv[])
-{
 	ws2811_return_t ret;
 
 	ledstring.freq = TARGET_FREQ;
@@ -211,5 +114,4 @@ int main(int argc, char *argv[])
 	//
 	//printf("\n");
 	return ret;
->>>>>>> 97926c9fc141408080e50ebfcbaff87ca49489f7
 }
